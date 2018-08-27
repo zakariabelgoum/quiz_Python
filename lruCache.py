8b13c250f4e973f29ce62de239df6813d74087f2
zakariabelgoum <zakariabelgoum94@gmail.com>
2018-08-27 06:01:56 -0400

    LRU cache

--- lruCache.py
+++ lruCache.py
@@ -1,52 +1,57 @@
+############## Code by Zakaria Belgoum ###########
+#lruCache implements a true LRU cache
+#Pylru provides a cache class with a simple dict interface.
+#Basic operations are get and set items
+
+
 import collections
 from datetime import datetime
 import time
 
 class LRUCache:
-
-    ''' we use an OrdredDict object that is a dictionary that remembers the order in which its contents are added '''
-    def __init__(self, capacity,timeExpiration):
+   
+    def __init__(self, capacity):
         self.capacity = capacity 
-        self.cachDictionary = collections.OrderedDict() # ordered dictionary that order items in cachDictionary
-        self.timeExpiration=timeExpiration
-        self.timestamp=datetime.now()
-
+        self.cacheDictionary = collections.OrderedDict() #we use an ordered dictionary that remembers the order entries were added
+        
+           
     def set(self, key, value):
         try:
-            self.cachDictionary.pop(key) #remove the item from dictionary
+            self.cacheDictionary.pop(key)  
         except KeyError:
-            if len(self.cachDictionary) >= self.capacity:
-                self.cachDictionary.popitem(last=False) #remove the least used item
-        self.cachDictionary[key] = value
-        self.timeExpiration +=1
+            if len(self.cacheDictionary) >= self.capacity:
+                self.cacheDictionary.popitem(last=False) 
+        self.cacheDictionary[key] = value
+        
 
     def get(self, key):
         try:
-            value = self.cachDictionary.pop(key) #remove the item and get the value that corresponds to the key
-            self.cachDictionary[key] = value
+            value = self.cacheDictionary.pop(key) 
+            self.cacheDictionary[key] = value 
             return value
         except KeyError:
             return -1
-        self.timeExpiration +=1
+        
+    #To do list
+    #add function to validate expiration of the cache
+    #add geo distributed architecture
+    #add spacial reference
+    #add replication of data
+    #writing in real time
 
-    #To do add function to verify time expiration for the cache
-    def verifyCacheExpiration(self):
-        ''' verify if the cache is used within the time of expiration time '''
-        now=datetime.now()
-        delta=now-self.timestamp
-        if delta.seconds > self.timeExpiration:
-            print('time has expired')
-            cachDictionary.clear()
-    
-#implementing tests
-lru = LRUCache(4,5)
-lru.set(1,'one')
-lru.set(2,'two')
-lru.set(3,'three')
-print(lru.get(1))
-lru.set(4,'four')
-lru.set(5,'five')
-print(lru.get(2))
+
+if __name__ == '__main__' :
+
+    lru = LRUCache(4)
+    lru.set(1,'one')
+    lru.set(2,'two')
+    lru.set(3,'three')
+    lru.set(4,'four')
+    lru.set(5,'five')
+    print(dict(lru.cacheDictionary))
+    lru.get(2)
+    print(dict(lru.cacheDictionary))
+
 
 
 
