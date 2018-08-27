ef4092e1ab99bd929571e6067e1d6fc3a3dda93b
zakariabelgoum <zakariabelgoum94@gmail.com>
2018-08-27 05:58:29 -0400

    overlap

--- overlap.py
+++ overlap.py
@@ -1,28 +1,27 @@
-################ code by Zakaria Belgoum #############
+################ code by Zakaria Belgoum ###############
+# we suppose that (x1,x2),(x3,x4) are two sorted tuples
+# (x1,x2) and (x3,x4) overlaps when x3 <= x1 <= x4 or x1 <= x3 <= x2
+########################################################
+
 
 def sort_tuple(my_tuple):
-	''' this function sort tuples 
-	sort_tuple((5,2))= (2,5)
-	sort_tuple((1,2))=(1,2) '''
 	sortedtuple=sorted(my_tuple)
 	return sortedtuple
 
 
-def isOverlap(input1,input2):
-	''' input1 and input2 should be firstly sorted 
-	there is overlap when the first element of one of the inputs is between the two elements of the other input '''
+def is_overlap(input1,input2):
 	sortedInput1=sort_tuple(input1)
 	sortedInput2=sort_tuple(input2)
-	if    sortedInput1[0] <=sortedInput2[0]<= sortedInput1[1] or sortedInput2[0] <=sortedInput1[0]<= sortedInput2[1]:
-		print(' %s and %s overlaps '%(input1,input2))
-
+	if sortedInput1[0] <=sortedInput2[0]<= sortedInput1[1] or sortedInput2[0] <=sortedInput1[0]<= sortedInput2[1]:
+		return '{} and {} overlaps'.format(input1,input2)
 	else :
-		print(' %s and %s doesnt overlap '%(input1,input2))
+		return '{} and {} doesnt overlap'.format(input1,input2)
 
-#testing
-isOverlap((1,2),(1,3))
-isOverlap((2,5),(1,3))
-isOverlap((9,5),(10,12))
+
+
+if __name__ == "__main__":
+	print(is_overlap((1,2),(1,3)))
+	print(is_overlap((9,5),(10,12)))
 
 
 
